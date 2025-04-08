77e9d543600ff475256b7d9756337f71d8ad317f
Jzjerry <jzjerry@foxmail.com>
2025-04-08 20:37:19 +0800

    mod mpq train with args

--- examples/mpq_train.py
+++ examples/mpq_train.py
@@ -2,22 +2,34 @@ import torch
 import numpy as np
 
 import sys
+import argparse
 
 from models import model_zoo
 
-model_name = sys.argv[1]
-epoches = sys.argv[2]
+argsparse = argparse.ArgumentParser()
+argsparse.add_argument("model_name", type=str)
+argsparse.add_argument("epoches", type=int, default=40)
+argsparse.add_argument("--batch-size", type=int, default=32)
+argsparse.add_argument("--lr", type=float, default=0.001)
+argsparse.add_argument("--scheduler", type=str, default="none")
+
+args = argsparse.parse_args()
+batch_size = args.batch_size
+model_name = args.model_name
+epoches = args.epoches
+lr = args.lr
+scheduler = args.scheduler
 
 if __name__ == "__main__":
 
     model, train_loader, test_loader = model_zoo.from_zoo(
-        model_name, shuffle=True, batch_size=128)
+        model_name, shuffle=True, batch_size=batch_size)
 
     res = model.train_loop(n_epoch=int(epoches),
                         train_loader=train_loader, 
                         test_loader=test_loader,
-                        lr = 0.1, 
-                        scheduler = "cifar100-step",
+                        lr = lr, 
+                        scheduler = scheduler,
                         early_stopping = False,
                         verbose=True)
     
