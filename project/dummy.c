04f4f1240a1f33815ed4a101f246258bc5c2a126
Jzjerry <jzjerry@foxmail.com>
2025-09-02 17:06:10 +0800

    add dummy & lib update

--- project/dummy.c
+++ project/dummy.c
@@ -8,9 +8,9 @@
 #include "mico_nn.h"
 #include "mico_qnn.h"
 
-#define N 16
-#define K 16
-#define M 16
+#define N 32
+#define K 32
+#define M 32
 
 int main(){
     
@@ -26,13 +26,27 @@ int main(){
     w.shape[0] = M;
     w.shape[1] = K;
 
-    int32_t *o = (int32_t*)malloc(N*M*sizeof(int32_t));
-    long start_time, end_time;
-    printf("MiCo 8x8 MatMul Test\n");
-    start_time = MiCo_time();
-    MiCo_Q8_MatMul(o, &x, &w);
-    end_time = MiCo_time();
-    printf("MiCo 8x8 Time: %ld\n", end_time - start_time);
+    for (size_t i = 0; i < N*K; i++) {
+        x.data[i] = (i % 8);
+    }
+    for (size_t i = 0; i < K*M; i++) {
+        w.data[i] = (i + 7 % 8);
+    }
+    #ifdef SOC
+    while(1){
+    #endif 
+        int32_t *o = (int32_t*)malloc(N*M*sizeof(int32_t));
+        long start_time, end_time;
+        printf("MiCo 8x8 MatMul Test\n");
+        start_time = MiCo_time();
+        MiCo_Q8_MatMul(o, &x, &w);
+        end_time = MiCo_time();
+        printf("MiCo 8x8 Time: %ld\n", end_time - start_time);
+        free(o);
+    #ifdef SOC
+        delay(1000); // 1 second
+    }
+    #endif
 
     return 0;
 }
